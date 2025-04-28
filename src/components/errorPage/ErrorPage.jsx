443a1445772b9a6808b681fcf2ae983c150d52a5
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-04-28 17:22:38 +0200

    feat: create error page

--- src/components/errorPage/ErrorPage.jsx
+++ src/components/errorPage/ErrorPage.jsx
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom';
-import './errorPage.module.css';
+import styles from './errorPage.module.css';
 
 export default function ErrorPage() {
   return (
-    <div className="errorPage">
-      <h1>404</h1>
-      <p>Oops, it looks like the page you are looking for does not exist</p>
-      <Link to="/">Go back home</Link>
+    <div className={styles.page}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.text}>
+        Oops, it looks like the page you are looking for does not exist
+      </p>
+      <button className={styles.button}>
+        <Link to="/" className="link">
+          Go back home
+        </Link>
+      </button>
     </div>
   );
 }
