3fbe9791fcd40dc673d2a7c43c77d105e4cdee11
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-04-29 16:26:08 +0200

    feat: create hook to fetch items from shop api

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,15 +1,39 @@
-import './shop.module.css';
+import { useState, useEffect } from 'react';
 import styles from './shop.module.css';
 
-let items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function useItems() {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('server error');
+        }
+        return response.json();
+      })
+      .then((response) => setItems(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { items, error, loading };
+}
 
 export default function Shop() {
+  const { items, error, loading } = useItems();
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>A network error was encountered</h1>;
+
   return (
     <div className={styles.shop}>
       <h1>Browse the following items</h1>
       <div className={styles.items}>
         {items.map((item) => {
-          return <p>{item}</p>;
+          return <p>{item.title}</p>;
         })}
       </div>
     </div>
