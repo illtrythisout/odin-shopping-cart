aab1970d7e13c4d5c1fc51bdd0667e1d99697637
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-04-29 16:14:44 +0200

    feat: create basic shop page

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,5 +1,17 @@
 import './shop.module.css';
+import styles from './shop.module.css';
+
+let items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default function Shop() {
-  return <h1>This is the 'Shop' page</h1>;
+  return (
+    <div className={styles.shop}>
+      <h1>Browse the following items</h1>
+      <div className={styles.items}>
+        {items.map((item) => {
+          return <p>{item}</p>;
+        })}
+      </div>
+    </div>
+  );
 }
