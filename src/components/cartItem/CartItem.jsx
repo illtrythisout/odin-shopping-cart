7c40eaa5f1af60c1746f59a2774cc4ea592e43c0
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-04-30 15:07:42 +0200

    fix: cart now shows total price of all items together

--- src/components/cartItem/CartItem.jsx
+++ src/components/cartItem/CartItem.jsx
@@ -5,7 +5,7 @@ export default function CartItem({ item, count }) {
     <div className={styles.item}>
       <img src={item.image} alt="" className={styles.image} />
       <span className={styles.title}>{item.title}</span>
-      <span className={styles.price}>{item.price}€</span>
+      <span className={styles.price}>{`Total: ${item.price * count}€`}</span>
       <span className={styles.quantity}>{`quantity: ${count}`}</span>
     </div>
   );
--- src/components/shopItem/ShopItem.jsx
+++ src/components/shopItem/ShopItem.jsx
@@ -11,7 +11,7 @@ export default function ShopItem({ item, addItemToCart }) {
     <div className={styles.item}>
       <img src={item.image} alt="" className={styles.image} />
       <span className={styles.title}>{item.title}</span>
-      <span className={styles.price}>{item.price}€</span>
+      <span className={styles.price}>{`${item.price}€`}</span>
       <div className={styles.purchaseActions}>
         <input
           id={`item${item.id}`}
